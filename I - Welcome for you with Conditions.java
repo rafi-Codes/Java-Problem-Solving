a8c557e9a5ca0a2b3b11574e3e27e3ee45ae94e5
Mohammad Rafiul Hasan <rafihasan27rafi@gmail.com>
2025-10-01 15:12:11 +0600

    Create I - Welcome for you with Conditions.java

---
+++ I - Welcome for you with Conditions.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+ 
+public class codeforces {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        if (num1 > num2 || num1 == num2){
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
+        sc.close();
+    }
+}
