a1106c81269ce17d4112e5b3f552c07c5553347b
Mohammad Rafiul Hasan <rafihasan27rafi@gmail.com>
2025-10-01 15:06:52 +0600

    Create E - Area of a Circle.java

---
+++ E - Area of a Circle.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+ 
+public class codeforces{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		double r = sc.nextDouble();
+		double pi = 3.141592653;
+		double area = pi * r * r;
+		System.out.println(area);
+		sc.close();
+	}
+}
