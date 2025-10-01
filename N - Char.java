5bdcf58deac2bfbb95723bfba49594c8388fb8e4
Mohammad Rafiul Hasan <rafihasan27rafi@gmail.com>
2025-10-01 15:22:19 +0600

    Create N - Char.java

---
+++ N - Char.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+ 
+public class codeforces {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+        if (Character.isUpperCase(ch)){
+            System.out.println(Character.toLowerCase(ch)); 
+        } else {
+            System.out.println(Character.toUpperCase(ch));
+        }
+        sc.close();
+    }
+}
